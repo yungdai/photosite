1cfdaacf169234cd55e080973275069fe1d47be8
Yung Dai <yungchidai@gmail.com>
2015-01-05 15:04:03 -0500

    added some more fields into the initial user database.

--- db/migrate/20150105012459_sorcery_core.rb
+++ db/migrate/20150105012459_sorcery_core.rb
@@ -4,7 +4,9 @@ class SorceryCore < ActiveRecord::Migration
       t.string :email,            :null => false
       t.string :crypted_password, :null => false
       t.string :salt,             :null => false
-
+      t.boolean :administrator
+      t.string  :first_name
+      t.string  :last_name
       t.timestamps
     end
 
